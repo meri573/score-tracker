021c0b761fbe016ef904a7bd50184eef4ea3e089
Meri <meri.kekalainen@gmail.com>
2026-09-08 14:04:24 +0300

    made score submission page and result page work

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/submit_score")
+def submit_score():
+    return render_template("submit_score.html")
+
+@app.route("/result", methods=["POST"])
+def result():
+    game = request.form["game"]
+    extras = request.form.getlist("extra")
+    description = request.form["description"]
+    return render_template("result.html", game=game, extras=extras, description=description)
