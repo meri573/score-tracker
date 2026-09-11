663a677f75eea5a8d0be36e76f4bd0d3deb823bb
Meri <meri.kekalainen@gmail.com>
2026-09-11 16:22:25 +0300

    added rudimentary results table to db schema

--- schema.sql
+++ schema.sql
@@ -4,3 +4,11 @@ CREATE TABLE users (
     password_hash TEXT
 );
 
+CREATE TABLE results (
+    id INTEGER PRIMARY KEY,
+    game TEXT,
+    big_mode INTEGER,
+    20G_mode INTEGER,
+    submitted_at TEXT,
+    user_id INTEGER REFERENCES users
+)
