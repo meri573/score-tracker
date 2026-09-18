6ab970303b7b950d2c66d05050fdb6e52b6276ae
Meri <meri.kekalainen@gmail.com>
2026-09-18 17:04:25 +0300

    added searching function to result_handler

--- result_handler.py
+++ result_handler.py
@@ -24,3 +24,10 @@ def update_result(result_id, description):
 def delete_result(result_id):
     sql = "DELETE FROM results WHERE id = ?"
     db.execute(sql, [result_id])
+
+def search_results(query):
+    sql = """SELECT u.username, r.id, r.game, r.time, r.grade, r.score, r.twentyg_mode, r.big_mode, r.submitted_at
+            FROM users u, results r  
+            WHERE r.user_id = u.id AND (u.username LIKE ? OR r.game LIKE ? OR r.time LIKE ? OR r.grade LIKE ? OR r.score LIKE ? OR r.submitted_at LIKE ?)"""
+    like = "%" + query + "%"
+    return db.query(sql, [like, like, like, like, like, like])
